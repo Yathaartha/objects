3132f79c5472f379c3df45e542a8279fa1781f23
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-26 21:40:07 +0545

    Dynamic object properties

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,3 +1,5 @@
+const userChosenKeyName = "level";
+
 let person = {
   "first name": "Bruce", //all strings are valid key names if enclosed in brackets. still dont use white spaces.
   age: 30,
@@ -5,7 +7,10 @@ let person = {
   greet: function () {
     alert("Hi there!");
   },
+  1.5: "hello",
 };
+
+const keyName = "first name";
 
 delete person.age; //delete properties
 // person.age = undefined;//works but dont use. not recommended.
@@ -15,3 +20,6 @@ person.age = null; //sets age to null
 person.isAdmin = true; //adding properties
 
 console.log(person["first name"]); //calling these keys use special syntax
+console.log(person[keyName]); //alternate way to call
+console.log(person[1.5]);
+// console.log(person["1.5"]);//also works.
