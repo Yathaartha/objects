38e65bd600210d9cb64b44e5d2800be98359c30e
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-27 17:41:23 +0545

    Object destructuring

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -40,10 +40,13 @@ function renderMovies(filter = "") {
 
   filteredMovies.forEach((movie) => {
     const movieEl = document.createElement("li");
-    let text = movie.info.title + " - ";
-    for (const key in movie.info) {
+    const { info, ...otherProps } = movie;
+    const { title: movieTitle } = info;
+    console.log(otherProps);
+    let text = movieTitle + " - ";
+    for (const key in info) {
       if (key !== "title") {
-        text = text + `${key}: ${movie.info[key]}`;
+        text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
